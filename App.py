deaa0835f70f61bf6ee715aedff1c8af6ee02129
Maciej Palka <mac.pal@protonmail.com>
2024-11-20 15:42:29 +0100

    Moved current app to blueprint

--- App.py
+++ App.py
@@ -1,32 +1,11 @@
 import flask
-import Recomender
-recomender = Recomender.MovieRecomender("cleaned_10000.csv")
-recomender.select_features(['cast','director','keywords','overview','genres','title'])
-recomender.vectorise()
+from blueprints.public.public import public
+
 app = flask.Flask(__name__, template_folder='templates')
 # Set up the main route
-@app.route('/', methods=['GET'])
-def main():
-    # Number of items per sublist 
-    n = 3 
-    movies = recomender.get_popular_movies()
-    # Create sublists using list comprehension 
-    sublists = [movies[i:i + n] for i in range(0, len(movies),n)]
-    return flask.render_template('index.html',movies=sublists)
+app.register_blueprint(public)
 
-@app.route('/found', methods=['POST'])
-def recomendation():
-    m_name = " ".join(flask.request.form['movie_name'].split())
-    result_final = recomender.recomend(m_name)
-    movie = recomender.get_movie_data(m_name)
-    return flask.render_template('found.html',movies=result_final,search=movie[0])
 
-@app.route("/keywords",methods=['POST'])
-def keywords_recomendation():
-    if flask.request.method == 'POST':
-        keywords = " ".join(flask.request.form['keywords'].split())
-        result_final = recomender.get_keyword_recomendation(keywords)
-        return flask.render_template('found.html',movies=result_final,search_name=keywords)
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
     #app.run()
